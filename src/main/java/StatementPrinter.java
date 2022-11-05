0dca34bc947ad56d0012acfc597d004ef4a558a2
sabouquier <sacha.bouquier@etu.uca.fr>
2022-11-05 11:01:49 +0100

    method amount

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -9,15 +9,30 @@ public class StatementPrinter {
     StringBuffer sb=new StringBuffer(String.format("Statement for %s\n", invoice.customer));
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-    final String TRAGEDY = "tragedy";
-    final String COMEDY = "comedy";
-
-
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
       float thisAmount = 0;
+      thisAmount = amount(perf, play);
+      
+      // add volume credits
+      volumeCredits += Math.max(perf.audience - 30, 0);
+      // add extra credit for every five comedy attendees
+      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
-      switch (play.type) {
+      sb.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount ), perf.audience));
+      totalAmount += thisAmount;
+    }
+    sb.append(String.format("Amount owed is %s\n", frmt.format(totalAmount )));
+    sb.append(String.format("You earned %s credits\n", volumeCredits));
+    return sb.toString();
+  }
+
+
+  public float amount(Performance perf, Play play){
+    final String TRAGEDY = "tragedy";
+    final String COMEDY = "comedy";
+    float thisAmount = 0;
+    switch (play.type) {
         case TRAGEDY:
           thisAmount = 400;
           if (perf.audience > 30) {
@@ -34,19 +49,7 @@ public class StatementPrinter {
         default:
           throw new Error("unknown type: ${play.type}");
       }
-
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every five comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
-
-      // print line for this order
-      sb.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount ), perf.audience));
-      totalAmount += thisAmount;
-    }
-    sb.append(String.format("Amount owed is %s\n", frmt.format(totalAmount )));
-    sb.append(String.format("You earned %s credits\n", volumeCredits));
-    return sb.toString();
+    return thisAmount;
   }
 
 }
