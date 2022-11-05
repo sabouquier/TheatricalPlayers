1827d82eee0bf5fee98a280724ad711d1c0b6d0d
sabouquier <sacha.bouquier@etu.uca.fr>
2022-11-05 12:50:02 +0100

    Customer class creation

---
+++ src/main/java/Customer.java
@@ -0,0 +1,11 @@
+public class Customer {
+    
+    public Customer(String name){
+        this.name = name;
+        this.points = 0;
+    }
+
+    String name;
+    String nClient;
+    int points;
+}
